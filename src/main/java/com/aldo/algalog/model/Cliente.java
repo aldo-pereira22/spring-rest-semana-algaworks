be400399d3c9796766e3013589cf07e2f138c446
Aldo <aldopereira.gyn@gmail.com>
2023-03-15 20:08:48 -0300

    Incluindo Validações

--- src/main/java/com/aldo/algalog/model/Cliente.java
+++ src/main/java/com/aldo/algalog/model/Cliente.java
@@ -1,9 +1,13 @@
-package com.aldo.algalog.domain.model;
+package com.aldo.algalog.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,8 +23,18 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(min =4 ,max = 60)
 	private String nome;
+	
+	@Email
+	@NotBlank
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 
 	
