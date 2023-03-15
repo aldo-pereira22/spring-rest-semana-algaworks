53e22e4a9498eb73873497e713a5b41369bdc9f7
Aldo <aldopereira.gyn@gmail.com>
2023-03-15 11:44:34 -0300

    Criação da Model Cliente

--- src/main/java/com/aldo/algalog/controller/ClienteController.java
+++ src/main/java/com/aldo/algalog/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.aldo.algalog.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,26 +14,12 @@ import com.aldo.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("628854751212");
-		cliente1.setEmail("joão@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Aldo");
-		cliente2.setTelefone("628854751212");
-		cliente2.setEmail("aldo@gmail.com");
-		
-		Cliente cliente3 = new Cliente();
-		cliente3.setId(3L);
-		cliente3.setNome("Zé das couves");
-		cliente3.setTelefone("6288547512123334");
-		cliente3.setEmail("zé@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2, cliente3);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
