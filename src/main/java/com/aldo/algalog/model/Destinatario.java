051ed29f36ed7447aebac5bb74b2e6d2ee0ad8e0
Aldo <aldopereira.gyn@gmail.com>
2023-03-16 15:10:52 -0300

    Criando Classe Destinatario e Entrega

--- src/main/java/com/aldo/algalog/model/Destinatario.java
+++ src/main/java/com/aldo/algalog/model/Destinatario.java
@@ -1,6 +1,29 @@
 package com.aldo.algalog.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Embeddable
 public class Destinatario {
 
-    
+    @Column(name = "destinatario_nome")
+    private String nome;
+
+    @Column(name = "destinatario_logradouro")
+    private String logradouro;
+
+    @Column(name = "destinatario_numero")
+    private String numero;
+
+    @Column(name = "destinatario_complemento")
+    private String complemento;
+
+    @Column(name = "destinatario_bairro")
+    private String bairro;
+
 }
--- src/main/java/com/aldo/algalog/model/Entrega.java
+++ src/main/java/com/aldo/algalog/model/Entrega.java
@@ -1,16 +1,35 @@
 package com.aldo.algalog.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Entrega {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Cliente cliente;
+
+    @Embedded
     private Destinatario destinatario;
+
     private BigDecimal taxa;
+
+    @Enumerated(EnumType.STRING)
     private StatusEntrega status;
+
     private LocalDateTime data;
     private LocalDateTime dataFinalizacao;
 }
